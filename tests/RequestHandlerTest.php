aea1b268aef253a71a15eb9f6bbd8f3241c13fa0
Rogerio Vicente <rogerpvl@gmail.com>
2010-03-08 00:26:47 +0000

    Added more skeleton tests for some classes.

---
+++ tests/RequestHandlerTest.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once('PHPUnit/Framework.php');
+
+class RequestHandlerTest extends PHPUnit_Framework_TestCase
+{
+
+}
+?>
---
+++ tests/RequestTest.php
@@ -0,0 +1,22 @@
+<?php
+
+require_once('PHPUnit/Framework.php');
+
+class RequestTest extends PHPUnit_Framework_TestCase
+{
+	public function testGetClass()
+	{
+		
+	}
+	
+	public function testGetMethod()
+	{
+		
+	}
+	
+	public function testGetParam()
+	{
+		
+	}
+}
+?>
